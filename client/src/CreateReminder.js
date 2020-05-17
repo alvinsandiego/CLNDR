be371a4bee2fdd1032afe47afee7e84d67913e1a
mubi123 <mmussa@ucsd.edu>
2020-05-17 07:32:52 -0700

    Add files via upload

---
+++ client/src/CreateReminder.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import './CreateReminder.css'
+ 
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class CreateReminder extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: new Date()
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.startDate)
+  }
+ 
+  render() {
+    return (
+      <form onSubmit={ this.onFormSubmit }>
+       
+        <div className="form-group">
+        <p>Time & Date:</p>
+          <DatePicker className="datePicker"
+              selected={ this.state.startDate }
+              onChange={ this.handleChange }
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={20}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+          />
+          <div>
+          <p>Description:</p>
+                <form>
+                    <textarea class="textAreaDescrip" placeholder='Description'/>
+                </form>
+          </div>
+                <button>Submit</button>
+
+        </div>
+      </form>
+    );
+  }
+  
+}
+
+export default CreateReminder;
+
+
+
