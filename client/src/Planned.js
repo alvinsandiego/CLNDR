095c342277733a125c48ee9ff5372ea88cfe20a5
Anh Mac <anhnmac@gmail.com>
2020-05-17 18:18:37 -0700

    re-implemented 'Planned.js' and 'Reminders.js'

--- client/src/Planned.js
+++ client/src/Planned.js
@@ -1,34 +1,27 @@
-function Planned(props) {
-  const sidebar = (
-    <ul>
-      {props.events.map((event) =>
-        <li key={event.id}>
-          {event.title}
-        </li>
-      )}
-    </ul>
-  );
-  const content = props.events.map((event) =>
-    <div key={event.id}>
-      <h3>{event.title}</h3>
-      <p>{event.date}</p>
-    </div>
-  );
-  return (
-    <div>
-      {sidebar}
-      <hr />
-      {content}
-    </div>
-  );
-}
+import React, { Component } from 'react';
 
-const events = [
-  {id: 1, title: 'CSES GBM', date: 'September 10th, 2020 - 2:30 PM'},
-  {id: 2, title: 'Khosla Fanclub GBM', date: 'September 11th, 2020 - 2:30 PM'},
-  {id: 3, title: 'ZOOM Dungeon CS Students Mixer', date: 'October 25th, 2020 - 7:00 PM'}
-  ];
-ReactDOM.render(
-  <Blog events={events} />,
-  document.getElementById('root')
-);
+class Planned extends Component {
+
+    render() {
+
+        const PlannedEvents = [
+            {Events: 'Event #1 - MM/DD/YYYY'},
+            {Events: 'Event #2 - MM/DD/YYYY'},
+            {Events: 'Event #3 - MM/DD/YYYY'},
+            {Events: 'Event #4 - MM/DD/YYYY'},
+            {Events: 'Event #5 - MM/DD/YYYY'}
+
+        ];
+        
+        return (
+            <div>
+                <h1>Planned</h1>
+                <ul class="bulletedlist">
+                    {PlannedEvents.map((item, index) => {
+                        return <li>{item.Events}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
--- client/src/Reminders.js
+++ client/src/Reminders.js
@@ -1,36 +1,27 @@
-function Reminders(props) {
-  const sidebar = (
-    <ul>
-      {props.events.map((event) =>
-        <li key={event.id}>
-          {event.title}
-        </li>
-      )}
-    </ul>
-  );
-  const content = props.events.map((event) =>
-    <div key={event.id}>
-      <h3>{event.title}</h3>
-      <p>{event.date}</p>
-    </div>
-  );
-  return (
-    <div>
-      {sidebar}
-      <hr />
-      {content}
-    </div>
-  );
-}
+import React, { Component } from 'react';
 
-const posts = [
-  {id: 1, title: 'CSES GBM', date: 'September 10th, 2020 - 2:30 PM'},
-  {id: 2, title: 'Khosla Fanclub GBM', date: 'September 11th, 2020 - 2:30 PM'},
-  {id: 3, title: 'ZOOM Back-to-school Party', date: 'October 12th, 2020 - 5:00 PM'},
-  {id: 4, title: 'ZOOM CS Students Meet-up', date: 'October 15th, 2020 - 6:00 PM'},
-  {id: 5, title: 'ZOOM Dungeon CS Students Mixer', date: 'October 25th, 2020 - 7:00 PM'}
-];
-ReactDOM.render(
-  <Blog events={events} />,
-  document.getElementById('root')
-);
+class Reminders extends Component {
+
+    render() {
+
+        const RemindersEvents = [
+            {Events: 'Event #1 - MM/DD/YYYY'},
+            {Events: 'Event #2 - MM/DD/YYYY'},
+            {Events: 'Event #3 - MM/DD/YYYY'},
+            {Events: 'Event #4 - MM/DD/YYYY'},
+            {Events: 'Event #5 - MM/DD/YYYY'}
+
+        ];
+        
+        return (
+            <div>
+                <h1>Reminders</h1>
+                <ul class="bulletedlist">
+                    {RemindersEvents.map((item, index) => {
+                        return <li>{item.Events}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
