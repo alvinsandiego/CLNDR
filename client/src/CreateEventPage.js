fa86af33c89f529260f6b659b0cb1d1a40c99d47
Kevin Traw <ktraw2@gmail.com>
2020-05-21 15:55:53 -0700

    Update CreateEvent page

--- client/src/CreateEventPage.js
+++ client/src/CreateEventPage.js
@@ -4,29 +4,43 @@ import "./styles/CreateEventPage.css"
 class CreateEvent extends Component {
     render(){
         return(
-        <div class="center">
+        <div>
             <h2 class="center">Create Event</h2>
-            <form>
-                <label>Event Title:</label>
-                <input type="text" id="title" placeholder="Event Title"/><br/>
+            <form class="center create-event-form">
+                <div class="form-input">
+                    <label>Event Title:</label>
+                    <input type="text" id="title" placeholder="Event Title"/>
+                </div>
 
-                <label>Start Time:</label>
-                <input type="time" id="Time" /><br/>
+                <div class="date-time-grid">
+                    <label>Start Date:</label><label>Start Time:</label>
+                    <input type="date" id="start_date" /><input type="time" id="start_time" />
+                </div>
 
-                <label>Start Date:</label>
-                <input type="date" id="Date" /><br/>
+                <div class="date-time-grid">
+                    <label>End Date:</label><label>End Time:</label>
+                    <input type="date" id="end_date" /><input type="time" id="end_time" />
+                </div>
 
-                <label>Details:</label>
-                <textarea rows = "5" cols = "50" id="details" placeholder="Enter description here..."></textarea><br/>
+                <div class="form-input">
+                    <label>Details:</label>
+                    <textarea rows = "5" cols = "50" id="details" placeholder="Enter description here..."></textarea>
+                </div>
 
-                <label>Keywords:</label>
-                <textarea rows = "3" cols = "50" id="details"></textarea><br/>
+                <div class="form-input">
+                    <label>Keywords:</label>
+                    <textarea rows = "3" cols = "50" id="details"></textarea>
+                </div>
 
-                <label>Co-Hosts:</label>
-                <textarea rows = "1" cols = "50" id="details"></textarea><br/>
+                <div class="form-input">
+                    <label>Co-Hosts:</label>
+                    <textarea rows = "1" cols = "50" id="details"></textarea>
+                </div>
 
-                <label>Pictures or Videos</label>
-                <button type="button">Upload</button>
+                <div class="form-input">
+                    <label>Pictures or Videos</label>
+                    <button type="button">Upload</button>
+                </div>
 
                 <br/><br/>
                 <input type="submit" value="Submit"/>
