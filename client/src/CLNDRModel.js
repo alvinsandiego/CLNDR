1cfc8ccf94bf56de5dc25fb99463ce5edbbaf48d
Alexander <a4zhu@ucsd.edu>
2020-05-27 22:23:54 -0700

    Added textbox message (should work) for successful follow/unfollow

--- client/src/CLNDRModel.js
+++ client/src/CLNDRModel.js
@@ -52,12 +52,18 @@ function followHost(hostId, accountId) {
 	db.collection('user').doc(accountId).update({
 		following: admin.firestore.FieldValue.arrayUnion(hostId)
 	})
+	.then(function() {
+        	console.log("Successfully Followed!");
+    	})
 }
 
 function unfollowHost(hostId, accountId) {
 	db.collection('user').doc(accountId).update({
 		following: admin.firestore.FieldValue.arrayRemove(hostId)
 	})
+	.then(function() {
+        	console.log("Successfully Unfollowed!");
+    	})
 }
 
 function getHost(hostId) {
