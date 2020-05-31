07f6539a3e9057a55a89bf8cb60682f581432670
mubi123 <mmussa@ucsd.edu>
2020-05-30 19:53:53 -0700

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const {createAccount, readAccount} = require('./model/CLNDRModel');
+const {createAccount, readAccount, createEvent, updateEvent, deleteEvent} = require('./model/CLNDRModel');
 
 const express = require('express');
 const app = express();
@@ -14,3 +14,19 @@ app.get('/test', (req, res) => {
 app.get('/test2', (req, res) => {
     readAccount("testUser1");
 })
+
+app.get('/test3', (req,res) => {
+    res.send({express: 'Test successful! This page received data from expressjs.'});
+    createEvent("testUser33", "pass123", "What is this?", "hmm",
+    "testUser1", "pass123", "What is this?", "hmm");
+})
+
+/*
+app.get('/test4', (req,res) => {
+    updateEvent("0","Mubi123", "Hello", "Meow", "Test",
+    "Run", "pass123", "Wow", "Mhmmm");
+})
+
+app.get('/test5', (req,res) => {
+    deleteEvent("0");
+}) */
