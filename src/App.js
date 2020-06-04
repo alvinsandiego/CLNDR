dd6c3dea80e97b9451b3dad182a98231a800b0f2
Kevin Traw <ktraw2@gmail.com>
2020-06-04 00:58:10 -0700

    Add redirect when logged in and prettified the indentation

--- src/App.js
+++ src/App.js
@@ -19,11 +19,21 @@ class App extends Component {
 	constructor(props){
 		super(props);
 
+		const userToken = localStorage.getItem('jwtToken');
+		if (userToken !== null) {
+			axios.get('http://localhost:5000/accountInfo', {
+				headers: { Authorization: 'JWT ' + userToken },
+			}).then(response => {
+				if(response.data.success){
+					this.props.history.push('/calendar');
+				}
+			});
+		}
+
 		this.state = {
 			data: null,
-			 signin: false,
-	 
-		 };
+			signin: false,
+		};
 	}
 
 	handleLogin(){
@@ -45,91 +55,96 @@ class App extends Component {
                 else {
 					alert("Invalid Username or Password");
                 }
-            })
+            });
         }
-		
-
-
 	}
-
 
 	handleForgotPassword(){
 		user = document.getElementById('userN').value;
 	}
 
-
-
-
-
   	render() {
-		
-    		return (
-				  <div style={{backgroundColor: '#d6f3ff', height: 1000}}>
-					  <div style= {styles.centerDiv}>
-						  <img src={logo} style= {{width: 100, height: 100}}/>
-        		  <h1>Login</h1>
-					  </div>
-            <br />
-					  <div style= {styles.centerDiv}>
-      	      <h3>Returning Member</h3>
-					  </div>					
-					  <div style= {styles.centerDiv}>
-						  <label>Username &nbsp;</label>
-						  <input type="text" name='userName' id="userN"/>
-        	  </div>
-					  <br />
-        	  <div style= {styles.centerDiv}>
-						  <label>Password &nbsp;</label>
-						  <input type="password" name='passWord' id="passW"/>
-					  </div>
-					  <br /> 
-					  <div style= {styles.centerDiv}>
-              
-					      <button class= 'login_button' style= {{width: 130}} onClick={() => this.handleLogin()}>
-                  Login
-                </button>
-              
-              <a href="/forgotpassword">  
-                <button class= 'login_button' style= {{width: 130}} onClick={() => this.handleForgotPassword()}>
-                  Forgot Password
-                </button>
-              </a>
-        	  </div>
-				    <br />
-            <br />
-     		    <div style= {styles.centerDiv}>
-						  <h3>Prospective Member</h3>	            
-					  </div>
-					  <div style= {styles.centerDiv}>
-					    <p>To enjoy full personalized benefits of CLNDR, sign up for an account.</p>
-	          </div>
-					  <div style= {styles.centerDiv}>
-						  <a href="/createaccount">
-							  <button class= 'login_button' style= {{width: 130}}>
-                  Create Account
-                </button>
-					    </a>
-            </div>
-					  <br />
-            <br />
-	          <div style= {styles.centerDiv}>
-					    <h3>Guest</h3>
-					  </div>
-	          <div style= {styles.centerDiv}>
-					    <p>To simply explore scheduled events, continue as guest.</p>
-	          </div>
-				    <div style= {styles.centerDiv}>
-					    <a href="/calendar"><button class= 'login_button' style= {{width: 130}}>
-                Continue as Guest
-              </button></a>
-				    </div>
-				    <br />
-          </div>
+		return (
+			<div style={{backgroundColor: '#d6f3ff', height: 1000}}>
+				<div style= {styles.centerDiv}>
+					<img src={logo} style= {{width: 100, height: 100}}/>
+					<h1>Login</h1>
+				</div>
+
+				<br />
+
+				<div style= {styles.centerDiv}>
+					<h3>Returning Member</h3>
+				</div>
+
+				<div style= {styles.centerDiv}>
+					<label>Username &nbsp;</label>
+					<input type="text" name='userName' id="userN"/>
+				</div>
+
+				<br />
+
+				<div style= {styles.centerDiv}>
+					<label>Password &nbsp;</label>
+					<input type="password" name='passWord' id="passW"/>
+				</div>
+
+				<br />
+
+				<div style= {styles.centerDiv}>
+					<button class= 'login_button' style= {{width: 130}} onClick={() => this.handleLogin()}>
+						Login
+					</button>
+			
+					<a href="/forgotpassword">  
+						<button class= 'login_button' style= {{width: 130}} onClick={() => this.handleForgotPassword()}>
+							Forgot Password
+						</button>
+					</a>
+				</div>
+
+				<br />
+				<br />
+
+				<div style= {styles.centerDiv}>
+					<h3>Prospective Member</h3>	            
+				</div>
+				
+				<div style= {styles.centerDiv}>
+					<p>To enjoy full personalized benefits of CLNDR, sign up for an account.</p>
+				</div>
+				
+				<div style= {styles.centerDiv}>
+					<a href="/createaccount">
+						<button class= 'login_button' style= {{width: 130}}>
+							Create Account
+						</button>
+					</a>
+				</div>
+
+				<br />
+				<br />
+
+				<div style= {styles.centerDiv}>
+					<h3>Guest</h3>
+				</div>
+				
+				<div style= {styles.centerDiv}>
+					<p>To simply explore scheduled events, continue as guest.</p>
+				</div>
+				<div style= {styles.centerDiv}>
+					<a href="/calendar">
+						<button class= 'login_button' style= {{width: 130}}>
+							Continue as Guest
+						</button>
+					</a>
+				</div>
+				
+				<br />
+			</div>
  	   	);
-		}
-
-  }
-
+	}
+}
 
 const styles = {
   centerDiv: {
