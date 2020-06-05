4c72484a55d4536d6c286c03698ed4a4d01f060c
mubi123 <mmussa@ucsd.edu>
2020-06-05 01:56:50 -0700

    Add files via upload

--- server/routes/userInfo.js
+++ server/routes/userInfo.js
@@ -2,10 +2,14 @@ const Accounts = require('../model/accountsModel');
 
 module.exports = function(app) {
     app.get("/userInfo", (req, res) => {
+        //console.log("in userinfo");
+        //console.log(req);
         if (req.query.userID != undefined) {
             Accounts.readAccountByID(req.query.userID).then(documentSnapshot => {
                 if (documentSnapshot.exists) {
+                    console.log("in userINDOOOOO");
                     var data = documentSnapshot.data()
+                    console.log(data);
                     delete data.password;
                     delete data.sec_answer;
                     delete data.following;
@@ -18,14 +22,14 @@ module.exports = function(app) {
             });
         }
         else if (req.query.username != undefined) {
-            Accounts.readAccountByUsername(req.body.username).then(querySnapshot => {
+            Accounts.readAccountByUsername(req.query.username).then(querySnapshot => {
                 if (!querySnapshot.empty) {
                     const firstUser = querySnapshot.docs[0];
                     var userData = firstUser.data();
                     delete userData.password;
                     delete userData.sec_answer;
-                    delete userData.following;
-                    delete userData.planned_events;
+                    delete data.following;
+                    delete data.planned_events;
                     res.status(200).send({success: true, id: firstUser.ref.id, data: userData});
                 }
                 else {
