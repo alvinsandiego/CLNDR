6de8b8b4d35ae472c0d305b0c42bc925cce3d111
mubi123 <mmussa@ucsd.edu>
2020-06-05 01:11:38 -0700

    Add files via upload

--- src/EventPage.js
+++ src/EventPage.js
@@ -9,7 +9,6 @@ import moment from 'moment'
 class EventPage extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             planEventButtonColor: "",
             planEventButtonText: "",
@@ -33,53 +32,57 @@ class EventPage extends Component {
     /*Get Account Details*/
     componentDidMount = () =>{
         const { handle } = this.props.match.params;
-        console.log(this.props.match.params.id);
-        axios.get(`http://localhost:5000/EventPages/`,{
+        axios.get(`http://localhost:5000/getEvent`,{
             params: {
-                eventID: this.props.match.params.id
+                eventId: this.props.match.params.id
             }
         }).then(response => {
             //for host that created the event
             //to check if user has used the 
             let userToken = localStorage.getItem('jwtToken');
-            console.log(response.data.hostID)
             axios.get('http://localhost:5000/userInfo',{
-                body: {
-                   userID: response.data.hostID 
+                params: {
+                   userID: response.data.data.hostID
                 }
                 //need to figure this out tmw ask Kevin
             }).then(response =>{
                 console.log(response);
                 if(response.data.success){
-                    this.setState({
-                        hostName: response.data.data.org_name
-                    })
+                    if(response.data.data.org_name == null){
+                        this.setState({
+                            hostName: response.data.data.username
+                        })
+                    }
+                    else{
+                        this.setState({
+                            hostName: response.data.data.org_name
+                        })
+                    }
                   }
                   else{
-
                   }
             })
-        var theStartDate = new Date(response.data.start._seconds*1000);
-        var theEndDate = new Date(response.data.end._seconds*1000);
+
+        var theStartDate = new Date(response.data.data.start._seconds*1000);
+        var theEndDate = new Date(response.data.data.end._seconds*1000);
         console.log(response.data);
+        console.log("yabababababab");
+        console.log(response.data.data.interestCount);
         this.setState({
-            eventName: response.data.eventName,
+            eventName: response.data.data.eventName,
             eventStartDate: moment(theStartDate).format('LL'),
             eventStartTime: moment(theStartDate).format('LT'),
             eventEndDate: moment(theEndDate).format('LL'),
             eventEndTime: moment(theEndDate).format('LT'),
-            //interestCount: response.data.data.interestCount,
-            eventDescription: response.data.eventDescription,
+            eventDescription: response.data.data.eventDescription,
             eventID: this.props.match.params.id,
-            //startDate: response.data.start
+            interestCount: response.data.data.interestCount
             })
         }).catch(error => {
             console.log("in componentDidMount");
             console.log(error.data);
         }); 
-            console.log(this.state.eventName); 
 
-   
     //to check if user has used the 
     let userToken = localStorage.getItem('jwtToken');
 	if (userToken === null) {
@@ -110,37 +113,82 @@ class EventPage extends Component {
             }
             else{
                 this.setState({
-                    planEventButtonColor: "#b8b8b8",
-                    planEventButtonText: "Plan to attend event",
+                    planEventButtonColor: "#789ade",
+                    planEventButtonText: "Add to planned events",
                 })
             }
   }
 
     //Handles the button to add planned event
     handleClickPlanEvent(){
+        let userToken = localStorage.getItem('jwtToken');
         if(this.state.planEventButtonColor != "#b8b8b8"){
             this.setState({planEventButtonColor: "#b8b8b8"})
             this.setState({planEventButtonText: "Remove from planned events"})
-            axios.post("http://localhost:5000/planEvent?userID="+"&eventID="+this.props.eventID);
+            if (userToken !== null) {
+                axios.post("http://localhost:5000/planEvent",
+                {
+                    eventId: this.props.match.params.id,
+                },
+                {
+                    headers: { Authorization: 'JWT ' + userToken },
+                }).then(response => {
+                    if(response.data.success){
+                        console.log(this.state.interestCount+1);
+                        this.handleInterestCount(this.state.interestCount+1);
+                    }
+                })
+            }
         }
+
         else{
             this.setState({planEventButtonColor: "#789ade"})
             this.setState({planEventButtonText: "Add to planned events"})
-            axios.post("http://localhost:5000/unplanEvent?userID="+"&eventID="+this.props.eventID);
+            if (userToken !== null) {
+                axios.post("http://localhost:5000/unplanEvent",
+                {
+                    eventId: this.props.match.params.id,
+                },
+                {
+                    headers: { Authorization: 'JWT ' + userToken },
+                }).then(response => {
+                    if(response.data.success){
+                        console.log(this.state.interestCount+1);
+                        this.handleInterestCount(this.state.interestCount-1);
+                    }
+                    console.log(response);
+                })
+            }
         }
 
     }
+
+    handleInterestCount(value){
+        console.log(value);
+        let userToken = localStorage.getItem('jwtToken');
+        if (userToken !== null) {
+        axios.post('http://localhost:5000/incrementInterest', {
+            eventId: this.props.match.params.id,
+            interestCount: value
+        },{
+            headers: { Authorization: 'JWT ' + userToken }
+        }).then(response => {
+            console.log(response);
+        if (response.data.success) {
+            this.setState({
+                interestCount: value
+            })
+        }
+    }).catch(error => console.log(error));
+    }
+}
 
 
     render() {
 
         return (
-
-
             <div style={{ backgroundColor: '#d6f3ff', height: 1500 }}>
-
-                <NavBar/>
-
+            <NavBar/>
             <body>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway"/>
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
